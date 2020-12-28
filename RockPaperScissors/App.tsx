3425299cb2fec2d357c28a51bb97484b0b59103c
Örn Haraldsson <ornharalds@gmail.com>
2020-12-28 10:53:03 +0100

    app.tsx used for navigation

--- RockPaperScissors/App.tsx
+++ RockPaperScissors/App.tsx
@@ -1,81 +1,19 @@
-import React, { Component, useEffect, useState } from 'react';
-import MQTTService from './modules/MQTT.ts';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { Client, Message } from 'react-native-paho-mqtt';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import HomeScreen from './src/screens/HomeScreen';
 
 
-const game = {
-  weapons: ['rock', 'scissors', 'paper'],
-  winner: function (player) {
-    var win = new Map();
-    win[0] = 1;
-    win[1] = 2;
-    win[2] = 0;
-    return win[player];
-  },
-  loser: function (player) {
-    var lose = new Map();
-    lose[0] = 2;
-    lose[1] = 0;
-    lose[2] = 1;
-    return lose[player];
-  },
-};
-
-export default function App() {
-  const [state, setState] = useState({
-    winOrLose: 'No Info',
-    cpuScore: 0,
-    playerScore: 0,
-  });
-
-  const setWeaponText = (wp) => {
-    setState({
-      winOrLose: chooseWeapon(wp),
-    });
-  };
-  return (
-    <View style={styles.container}>
-      <Text>{state.winOrLose}</Text>
-      <Button style={styles.buttons} onPress={() => setWeaponText(0)} title="Rock" />
-      <Button style={styles.buttons} onPress={() => setWeaponText(1)} title="Scissors" />
-      <Button style={styles.buttons} onPress={() => setWeaponText(2)} title="Paper" />
-    </View>
-  );
+const navigator = createStackNavigator(
+ {   
+        Home: HomeScreen,
+ 
+ },
+ {
+    initialRouteName: "Home",
+        defaultNavigationOptions: {
+        title: "App"
+    }
 }
+);
 
-function battle(player, cpu) {
-  const mqtt = new MQTTService();
-  mqtt.getSend(game.weapons[player]);
-  console.log(game.weapons[player]);
-  console.log(game.weapons[cpu]);
-  if (game.winner(player) == cpu) {
-    console.log('win');
-    return 'Win';
-
-    } else if (game.loser(player) == cpu) {
-     console.log('lost');
-     return 'Lost';
-
-    }else {
-     console.log('draw');
-    return 'Draw';
-  }
-}
-
-function chooseWeapon(player) {
-  let cpu = Math.floor(Math.random() * 3);
-  return battle(player, cpu);
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttons: {
-    padding: 30,
-  },
-});
+export default createAppContainer(navigator);
